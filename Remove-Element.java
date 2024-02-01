8fc005a2f61659e3353ef1186730ef9a999ef302
Vikas Verma <110339905+Vikas-vgithub@users.noreply.github.com>
2024-02-01 13:33:56 +0530

    Create Remove-Element.java

---
+++ Remove-Element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int pointer = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                nums[pointer] = nums[i];
+                pointer++;
+            }
+        }
+        return pointer;
+    }
+}
